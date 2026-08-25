1d2b3cef557b2c9be7d593d37b0b3d15cdbae779
Flazened <ytmichaelyusliardy88@gmail.com>
2026-08-25 23:49:40 +0700

    chore: docker add httpd localhost

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Landing', function () {
+    return view('Home');
+});
+
+Route::get('/Landing/Menu', function () {
+    return view("Menu");
+});

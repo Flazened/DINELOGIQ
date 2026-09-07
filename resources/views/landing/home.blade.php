c93c0f4dff14a155913e9dc6e136e99c82f81b14
flazened <ytmichaelyusliardy88@gmail.com>
2026-09-07 16:41:37 +0700

    delete: refactor design, delete all code, following the figma design

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -16,17 +16,6 @@
         
 
         
-        <div class="relative z-10 h-[145vh] w-full"></div>
-        <!-- Tambahkan class relative dan z-10 agar naik ke atas background -->
-        <div class="relative z-10 -ml-20">
-            <img src="{{ asset('img/Logo/LogoWithName.png') }}" class="scale-40">
-            <p class="text-yellow-300 font-bold ml-60 -mt-15 mb-10 text-6xl">Enjoy the true<br>Japanese ramen soup</p>
-            <p class="text-white ml-60 text-3xl">Our soup distributed directly from Japan and <br>we guarantee you the best quality from it.</p>
-            <div class="ml-280 flex flex-row -mt-100">
-                <img src="{{ asset('img/Food/FoodOrang.png') }}" class="h-auto w-80 ml-10">
-                <img src="{{ asset('img/Food/Ramen-Wangy.png') }}" class="h-auto w-80 ml-10">
-            </div>        
-        </div>
 
 
 

96cb9477058c12cae0362a7b03b15b21f49803e8
flazened <ytmichaelyusliardy88@gmail.com>
2026-09-11 21:54:02 +0700

    refactor: changes menu image

--- resources/views/landing/menu.blade.php
+++ resources/views/landing/menu.blade.php
@@ -10,7 +10,7 @@
 </head>
     <body>
         @include('partials.landing.header')
-        <img src="{{ asset('') }}"
+        <img src="{{ asset('img/Background/Menu-Header-BG.png') }}"
              class="-mt-40 relative z-0 justify-centerd w-full bg-cover bg-center bg-no-repeat flex items-center">
         <div class="bg-[#F0EDE5] h-40 w-full flex justify-between px-20 items-center">
             <section class="bg-black w-40 h-20 rounded-[40px] flex justify-center items-center">
